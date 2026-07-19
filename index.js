1c186351794ef4b6a0930ea89ffe800ee13f7ede
rodrigo-cesar23 <rodrigocesarbusiness@gmail.com>
2026-07-19 20:48:40 -0300

    Desafio Classificador de nível de Herói

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-adadasda
+let nome = "rod";
+let nivel = "";
+let xp = 10001;
+
+if (xp <= 1000) {
+    nivel = "Ferro";
+} else if (xp <= 2000) {
+    nivel = "Bronze";
+} else if (xp <= 5000) {
+    nivel = "Prata";
+} else if (xp <= 7000) {
+    nivel = "Ouro";
+} else if (xp <= 8000) {
+    nivel = "Platina";
+} else if (xp <= 9000) {
+    nivel = "Ascendente";
+} else if (xp <= 10000) {
+    nivel = "Imortal";
+} else if (xp >= 10001) {
+    nivel = "Radiante";
+    }
+
+console.log("A Herói " + nome + " está no nível: " + nivel)
